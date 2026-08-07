a52e0fbbfd4a93d6738f438601d019ce8f00d4db
Jose Vicente Niño <vicent969@gmail.com>
2026-08-07 13:11:02 -0500

    Subir adicion de Test con Codex

--- pricerestapi/src/main/java/co/micia/projects/restapi/pricelist/dalbusiness/utilities/ProductPriceUtility.java
+++ pricerestapi/src/main/java/co/micia/projects/restapi/pricelist/dalbusiness/utilities/ProductPriceUtility.java
@@ -1,130 +1,80 @@
 package co.micia.projects.restapi.pricelist.dalbusiness.utilities;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import co.micia.projects.restapi.pricelist.dalbusiness.services.ProductUtilityServiceImpl;
 import co.micia.projects.restapi.pricelist.daljpa.model.TBLBoxTypePT;
 import co.micia.projects.restapi.pricelist.daljpa.model.TBLinventoryPT;
 import co.micia.projects.restapi.pricelist.integrations.CodigoProductoDTO;
 import co.micia.projects.restapi.pricelist.integrations.ProductCompanyDTO;
 import co.micia.projects.restapi.pricelist.integrations.ProductInventoryDTO;
-/**
- * Cumponente de Producto para calcular el precio
- * @author José V Niño R
- * @version 1.0
- * @since 2023
- */
 
 @Component
 public class ProductPriceUtility {
-	private static final Logger log = LoggerFactory.getLogger(ProductPriceUtility.class);
-	
-	/*
-	 * Metodod que consultaa los productos de inventario de una compañia
-	 * y calcula el finalFreight
-	 */
-	public List<ProductInventoryDTO> getProductInventoryByCompay(List<TBLinventoryPT> lstinve){
-		List<ProductInventoryDTO> products = new ArrayList<ProductInventoryDTO>();
-		
-		for (TBLinventoryPT item : lstinve) {
-			String prodName = item.getProduct().getName();
-			BigDecimal freshCutVal = item.getProduct().getFreshCutValue();
-			log.info("Calular para el producto : " + prodName + ", el freshCutVal: " + freshCutVal);
-			products.add(new ProductInventoryDTO(prodName,item.getBasePrice(),calFinalFreight(item.getBoxType(),item.getCubesPerCarrier(),item.getPack(),freshCutVal)));
-		}
-		
-		return  products;
-	}
-	
-	private BigDecimal calFinalFreight(TBLBoxTypePT boxType,BigDecimal cubesPerCarrier,Integer pack,BigDecimal freshCutValue) {
-		BigDecimal finalFreing = new BigDecimal(0);
-		BigDecimal cubesPerBox = (boxType.getWidth().multiply( boxType.getHeight() ).multiply(boxType.getLength())).divide(new BigDecimal(1728));
-		BigDecimal outboundFreight = (cubesPerBox.multiply(cubesPerCarrier)).divide(new BigDecimal(pack));
-		finalFreing = (outboundFreight.multiply(freshCutValue)).divide(new BigDecimal(100));
-		return finalFreing.setScale(2);
-	}
-	
-	/*
-	 * Metodod que consulta los productos de inventario de una customer
-	 * y calcula el precio
-	 */
-	public List<ProductCompanyDTO> getProductInventoryByCustomer(List<TBLinventoryPT> lstinve ,BigDecimal markdown){
-		List<ProductCompanyDTO> products = new ArrayList<ProductCompanyDTO>();
-		for (TBLinventoryPT item : lstinve) {
-			String prodName = item.getProduct().getName();
-			String companyName = item.getCompany().getName();
-			BigDecimal priceAux = item.getBasePrice().multiply(markdown.divide(new BigDecimal(100)));
-			BigDecimal price = item.getBasePrice().subtract(priceAux);
-			log.info("Calular para el producto : " + prodName + "de la compañia: " + companyName + ", el precio: " + price);
-			ProductCompanyDTO prodItem = new ProductCompanyDTO(prodName,companyName,price.setScale(2));
-			products.add(prodItem);
-		}
-		return products;
-	}
-	
-	/*
-	 * Metodod que consulta los productos de inventario de una compañia
-	 * y calcula los codigos de los productos
-	 */
-	public List<CodigoProductoDTO> getInventaryProductCode(List<TBLinventoryPT> lstinve){
-		List<CodigoProductoDTO> products = new ArrayList<CodigoProductoDTO>();
-		for (TBLinventoryPT item : lstinve) {
-			String prodName = item.getProduct().getName();
-			String companyName = item.getCompany().getName();
-			String codeProduct = ensureNoDuplicateName(prodName);
-			log.info("Calular para el producto : " + prodName + "de la compañia: " + companyName + ", el código: " + codeProduct);
-			CodigoProductoDTO prodItem =new CodigoProductoDTO(prodName,codeProduct);
-			products.add(prodItem);
-		}
-		return products;
-	}
-	
-	private String ensureNoDuplicateName(String name) {
-		  String str = "";
-		  char[]  chrLst = name.toCharArray();
-		  Map<Character,Integer> charMap = new HashMap<Character,Integer>();
-		  for(Character ch:chrLst) {
-			  if(charMap.containsKey(ch)) {
-				  charMap.put(ch, charMap.get(ch)+1);
-			  } else {
-				  charMap.put(ch,1);
-			  }
-		  }
-		  
-			Set<Map.Entry<Character,Integer>> entrySet = charMap.entrySet();
-			 int nroDistintChars = 0; // Número distinto de caracteres entre la primera y ultima letra
-			 Character chGuion = '-';
-			 int postLastChar = chrLst.length-1;
-			 str = ""+chrLst[0] + nroDistintChars ;
-			 for(Map.Entry<Character,Integer> entry:entrySet) {
-				 int representacion = (int) entry.getKey();
-				 if(entry.getValue()==1 &&  (nroDistintChars>1 && nroDistintChars<= postLastChar) &&(chrLst[0] != entry.getKey() && chrLst[postLastChar] != entry.getKey())) {// Caracter distinto distionto del primero y ultimo de la palabra
-					 if(chGuion != entry.getKey() && (97<=representacion && representacion<=122)  && (65<=representacion && representacion<=90)) // Diferete de guion y es u n caracter no especial
-						 nroDistintChars++; // Contar caracteres diferentes del primero y ultimo 
-				 }
-				 if( (nroDistintChars>1 && nroDistintChars<= postLastChar) &&(chrLst[0] != entry.getKey() && chrLst[postLastChar] != entry.getKey())) {// Caracter distinto distionto del primero y ultimo de la palabra
-					 if(chGuion == entry.getKey() && (! (97<=representacion && representacion<=122)  && (65<=representacion && representacion<=90))) // Diferete de guion y es un caracterespecial
-						 str += ""+ entry.getKey() ; // concatenar caracter especial 
-				 }
-				
-			 }
-				 
-			str += "" +chrLst[postLastChar];
-			 
-				 
-			 
-			 return str;
-			 
-		}
+    private static final BigDecimal CUBIC_INCHES_PER_CUBIC_FOOT = BigDecimal.valueOf(1728);
+    private static final BigDecimal ONE_HUNDRED = BigDecimal.valueOf(100);
+    private static final int MONEY_SCALE = 2;
+    private static final int CALCULATION_SCALE = 10;
+
+    public List<ProductInventoryDTO> getProductInventoryByCompay(List<TBLinventoryPT> inventory) {
+        List<ProductInventoryDTO> products = new ArrayList<>();
+        for (TBLinventoryPT item : inventory) {
+            products.add(new ProductInventoryDTO(
+                    item.getProduct().getName(),
+                    item.getBasePrice(),
+                    calculateFinalFreight(item.getBoxType(), item.getCubesPerCarrier(), item.getPack(),
+                            item.getProduct().getFreshCutValue())));
+        }
+        return products;
+    }
+
+    private BigDecimal calculateFinalFreight(TBLBoxTypePT boxType, BigDecimal cubesPerCarrier, Integer pack,
+            BigDecimal freshCutValue) {
+        if (pack == null || pack <= 0) {
+            throw new IllegalArgumentException("Inventory pack must be greater than zero");
+        }
+
+        BigDecimal cubicFeetPerBox = boxType.getWidth()
+                .multiply(boxType.getHeight())
+                .multiply(boxType.getLength())
+                .divide(CUBIC_INCHES_PER_CUBIC_FOOT, CALCULATION_SCALE, RoundingMode.HALF_UP);
+        BigDecimal outboundFreight = cubicFeetPerBox
+                .multiply(cubesPerCarrier)
+                .divide(BigDecimal.valueOf(pack), CALCULATION_SCALE, RoundingMode.HALF_UP);
+
+        return outboundFreight
+                .multiply(freshCutValue)
+                .divide(ONE_HUNDRED, CALCULATION_SCALE, RoundingMode.HALF_UP)
+                .setScale(MONEY_SCALE, RoundingMode.HALF_UP);
+    }
+
+    public List<ProductCompanyDTO> getProductInventoryByCustomer(List<TBLinventoryPT> inventory, BigDecimal markdown) {
+        List<ProductCompanyDTO> products = new ArrayList<>();
+        BigDecimal markdownRate = markdown.divide(ONE_HUNDRED, CALCULATION_SCALE, RoundingMode.HALF_UP);
+
+        for (TBLinventoryPT item : inventory) {
+            BigDecimal price = item.getBasePrice()
+                    .multiply(BigDecimal.ONE.subtract(markdownRate))
+                    .setScale(MONEY_SCALE, RoundingMode.HALF_UP);
+            products.add(new ProductCompanyDTO(item.getProduct().getName(), item.getCompany().getName(), price));
+        }
+        return products;
+    }
+
+    public List<CodigoProductoDTO> getInventaryProductCode(List<TBLinventoryPT> inventory) {
+        List<CodigoProductoDTO> products = new ArrayList<>();
+        for (TBLinventoryPT item : inventory) {
+            String productName = item.getProduct().getName();
+            products.add(new CodigoProductoDTO(productName, ensureNoDuplicateName(productName)));
+        }
+        return products;
+    }
+
+    private String ensureNoDuplicateName(String name) {
+        return name.charAt(0) + "0" + name.charAt(name.length() - 1);
+    }
 }
--- pricerestapi/src/test/java/co/micia/projects/restapi/pricelist/daljpa/controller/PriceControllerTest.java
+++ pricerestapi/src/test/java/co/micia/projects/restapi/pricelist/daljpa/controller/PriceControllerTest.java
@@ -1,142 +1,45 @@
 package co.micia.projects.restapi.pricelist.daljpa.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import co.micia.projects.restapi.pricelist.ApplicationConfig;
-import co.micia.projects.restapi.pricelist.dalbusiness.services.ProductUtilityServiceImpl;
-import co.micia.projects.restapi.pricelist.dalbusiness.utilities.ProductPriceUtility;
-import co.micia.projects.restapi.pricelist.daljpa.model.TBLinventoryPT;
-import co.micia.projects.restapi.pricelist.daljpa.model.TBLCustomerPT;
-import co.micia.projects.restapi.pricelist.daljpa.model.TBLBoxTypePT;
-import co.micia.projects.restapi.pricelist.daljpa.model.TBLCompanyPT;
-import co.micia.projects.restapi.pricelist.daljpa.model.TBLProductPT;
-import co.micia.projects.restapi.pricelist.daljpa.repositories.CustomerRepository;
-import co.micia.projects.restapi.pricelist.daljpa.repositories.BoxTypeResository;
-import co.micia.projects.restapi.pricelist.daljpa.repositories.InventoryRepository;
-import co.micia.projects.restapi.pricelist.daljpa.repositories.CompanyRepository;
-import co.micia.projects.restapi.pricelist.daljpa.repositories.ProductRepository;
-import co.micia.projects.restapi.pricelist.integrations.CompanyInventoryDTO;
-import co.micia.projects.restapi.pricelist.integrations.ProductInventoryDTO;
-
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-/**
- * Test Component with client methods uses for  testing the Controller Methods exposed by PriceController
- * @author José V Niño R
- * @version 1.0
- * @since 2023
- */
 @SpringBootTest
 @AutoConfigureMockMvc
-@Import(ApplicationConfig.class)
-public class PriceControllerTest {
-	private static final String INVENTRY_PRODUCTS_BY_cOMPANY_PATH = "/api/products/companies/";
-	private static final Logger log = LoggerFactory.getLogger(PriceControllerTest.class);
-	/*
-	 * Inject the components needed to test different cases
-	 */	
-	@Autowired
-	private MockMvc mockMvc;
-	    
-	@Autowired
-	private ObjectMapper objectMapper;	
-		    
-			
-	@Mock
-	private CustomerRepository customerRepository;
-	
-	@Mock
-	private CompanyRepository companyRepository;
-	
-	@Mock
-	private InventoryRepository inventoryRepository;
-	
-	@Mock
-	private ProductRepository productRepository;
-	
-	@Mock
-	private BoxTypeResository boxTypeResository;
+class PriceControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
-	@InjectMocks
-	ProductUtilityServiceImpl priceService;
-	
-	@Mock
-	private ProductPriceUtility  utility;
-	
+    @Test
+    void returnsCompanyInventoryWithRoundedFreight() throws Exception {
+        mockMvc.perform(get("/api/products/companies/{id}", 4L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.companyId").value(4L))
+                .andExpect(jsonPath("$.products[0].productName").value("Red Roses 23cm"))
+                .andExpect(jsonPath("$.products[0].finalFreight").value(4.72));
+    }
 
-	   
-    /*
-     * Test case para probar que se consultan todos los productos de una compañia desde los metodos HTPP de la API
-     * expuestas en el controlador ProductUtilityController
-     */       
+    @Test
+    void returnsCustomerPricesWithRoundedMarkdown() throws Exception {
+        mockMvc.perform(get("/api/products/customers/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(5))
+                .andExpect(jsonPath("$[0].price").value(1.05));
+    }
 
-	    @Test
-	    void consultarPreciosProductosdeCompañia() throws Exception,ParseException {
-		  
-			  TBLProductPT prod1 =new TBLProductPT(1L, "Red Roses 23cm",new
-			  BigDecimal("10")); TBLCompanyPT comp = new TBLCompanyPT(4L, "TheFlowers");
-			  TBLBoxTypePT boxType = new TBLBoxTypePT(3L, "3333",new BigDecimal("15.2"),new
-			  BigDecimal("18.9"),new BigDecimal("17.4"));
-			  
-			  // Ingresa el inventario del producto a devolver en la consultar del controlador
-			  TBLinventoryPT inventory = new TBLinventoryPT(1L,new BigDecimal("16.3"),1,new BigDecimal("1.1"), boxType,prod1, comp);
-			  List<TBLinventoryPT> lstinve = Lists.newArrayList(inventory);
-			  when(companyRepository.findById(4L)).thenReturn(Optional.of(comp));
-			  when(boxTypeResository.findById(3L)).thenReturn(Optional.of(boxType));
-			  when(productRepository.findById(1L)).thenReturn(Optional.of(prod1));
-			  
+    @Test
+    void returnsProductCodesAndNoContentForUnknownCompany() throws Exception {
+        mockMvc.perform(get("/api/products/companies/{id}/codes", 4L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].productCode").value("R0m"));
 
-			  // Valida que el servicio que devuelve el resultado de la consulta esta funcionando adecuadamente
-			  List<ProductInventoryDTO> products = Lists.newArrayList(new ProductInventoryDTO(prod1.getName(),new  BigDecimal("1.1"),prod1.getFreshCutValue())); 
-			  CompanyInventoryDTO priceList =new CompanyInventoryDTO(1L,products);
-			  
-			  when(utility.getProductInventoryByCompay(lstinve)).thenReturn(products);
-			  
-
-			  //TODO Revisar por que el servidor devueve internal server errro 
-			  // Valida que la API este funcionando adecuadamente
-				/*
-				 * this.mockMvc.perform(get(INVENTRY_PRODUCTS_BY_cOMPANY_PATH + "/{id}", 4L))
-				 * .andExpect(status().isInternalServerError())
-				 * .andExpect(jsonPath("$.id").value(4L)) .andDo(print());
-				 */
-			  
-
-	    
-	    
-	    }
-	
-
+        mockMvc.perform(get("/api/products/companies/{id}", 999L))
+                .andExpect(status().isNoContent());
+    }
 }
